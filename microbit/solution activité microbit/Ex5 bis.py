6f3b0446e9a0b1fc2ee7628fe44af45af6818bcd
Sp8ceranger <Spacerangerdidi@outlook.com>
2026-04-28 18:52:37 +0200

    Create Ex5 bis.py

---
+++ microbit/solution activité microbit/Ex5 bis.py
@@ -0,0 +1,10 @@
+for index in range(10):
+    basic.show_icon(IconNames.HEART)
+    basic.show_leds("""
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        """)
+    basic.pause(500)
