99a2ffb7847a1654cf45628dfc9f1295d9ec6c93
Sp8ceranger <Spacerangerdidi@outlook.com>
2026-04-29 09:02:54 +0200

    Create Ex12.py

---
+++ microbit/solution activité microbit/Ex12.py
@@ -0,0 +1,36 @@
+NOMBRE = 0
+
+def on_gesture_shake():
+    global NOMBRE
+    NOMBRE = randint(0, 2)
+    if NOMBRE == 1:
+        basic.show_leds("""
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            """)
+    elif NOMBRE == 2:
+        basic.show_leds("""
+            # # . # #
+            # # . # #
+            . . # . .
+            . # . # .
+            # . . . #
+            """)
+    elif NOMBRE == 3:
+        basic.show_leds("""
+            . # # # .
+            # . . . #
+            # . . . #
+            # . . . #
+            . # # # .
+            """)
+    basic.pause(500)
+    basic.clear_screen()
+input.on_gesture(Gesture.SHAKE, on_gesture_shake)
+
+def on_forever():
+    pass
+basic.forever(on_forever)
